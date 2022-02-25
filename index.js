a18e59d250460ee7c9768b46a40749881be0fed9
ExCaliBur2020 <yashraut361@gmail.com>
2022-02-25 22:22:26 +0530

    v1

---
+++ index.js
@@ -0,0 +1,29 @@
+function getThumbnail(link, quality) {
+    const videoid = youtube_parser(link)
+    if (videoid) {
+        switch (quality) {
+            case 'max':
+                return `https://i1.ytimg.com/vi/${videoid}/maxresdefault.jpg`
+            case 'hq':
+                return `https://i1.ytimg.com/vi/${videoid}/mqdefault.jpg`
+            case 'mq':
+                return `https://i1.ytimg.com/vi/${videoid}/hqdefault.jpg`
+            case 'sd':
+                return `https://i1.ytimg.com/vi/${videoid}/sddefault.jpg`
+            case 'default':
+                return `https://i1.ytimg.com/vi/${videoid}/default.jpg`
+            default:
+                return 'Please provide proper input for quality (max,hq,mq,sd,default)'
+        }
+    } else {
+        return 'Please check youtube video link'
+    }
+}
+
+function youtube_parser(url) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match && match[7].length == 11) ? match[7] : false;
+}
+
+module.exports = getThumbnail;
