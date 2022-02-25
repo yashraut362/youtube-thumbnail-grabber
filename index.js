95e568374392614634d19a1dab18055bdc1b3aa0
ExCaliBur2020 <yashraut361@gmail.com>
2022-02-25 22:50:42 +0530

    improved docs

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 function getThumbnail(link, quality) {
-    const videoid = youtube_parser(link)
-    if (videoid) {
-        switch (quality) {
-            case 'max':
-                return `https://i1.ytimg.com/vi/${videoid}/maxresdefault.jpg`
-            case 'hq':
-                return `https://i1.ytimg.com/vi/${videoid}/mqdefault.jpg`
-            case 'mq':
-                return `https://i1.ytimg.com/vi/${videoid}/hqdefault.jpg`
-            case 'sd':
-                return `https://i1.ytimg.com/vi/${videoid}/sddefault.jpg`
-            case 'default':
-                return `https://i1.ytimg.com/vi/${videoid}/default.jpg`
-            default:
-                return 'Please provide proper input for quality (max,hq,mq,sd,default)'
+    if (link && quality) {
+        const videoid = youtube_parser(link)
+        if (videoid) {
+            switch (quality) {
+                case 'max':
+                    return `https://i1.ytimg.com/vi/${videoid}/maxresdefault.jpg`
+                case 'hq':
+                    return `https://i1.ytimg.com/vi/${videoid}/mqdefault.jpg`
+                case 'mq':
+                    return `https://i1.ytimg.com/vi/${videoid}/hqdefault.jpg`
+                case 'sd':
+                    return `https://i1.ytimg.com/vi/${videoid}/sddefault.jpg`
+                case 'default':
+                    return `https://i1.ytimg.com/vi/${videoid}/default.jpg`
+                default:
+                    return 'Please provide proper input for quality (max,hq,mq,sd,default)'
+            }
+        } else {
+            return 'Please check youtube video link'
         }
     } else {
-        return 'Please check youtube video link'
+        return 'Please provide link and quality'
     }
 }
 
@@ -25,5 +29,8 @@ function youtube_parser(url) {
     var match = url.match(regExp);
     return (match && match[7].length == 11) ? match[7] : false;
 }
+
+
+console.log(getThumbnail('https://www.youtube.com/watch?v=uHklxa2UVh0', 'max'))
 
 module.exports = getThumbnail;
